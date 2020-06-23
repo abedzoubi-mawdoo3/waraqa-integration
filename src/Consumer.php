7bad90077bf031983118b2c66f9a5a20126fda14
Mohammad Suleiman <mohammad.suleiman@mawdoo3.com>
2020-06-23 13:10:57 +0300

    v1.04 fixing wrong channels binding

--- src/Consumer.php
+++ src/Consumer.php
@@ -11,9 +11,10 @@ class Consumer
      *
      * @param  mixed $WConnection
      * @param  mixed $callback
+     * @param  bool $check
      * @return void
      */
-    public function consume(WAMQPConnect $WConnection, $callback)
+    public function consume(WAMQPConnect $WConnection, $callback, $check=true)
     {
         $connection = $WConnection->connection;
         $channel = $connection->channel();
@@ -23,12 +24,14 @@ class Consumer
         $channel->exchange_declare($WConnection->exchange, 'direct', false, true, false);
         $channel->queue_bind($WConnection->queue, $WConnection->exchange);
 
-        $channel->basic_consume($WConnection->queue, 'consumer_tag', false, false, false, false, $callback);
+        $channel->basic_consume($WConnection->queue, 'consumer_tag', false, true, false, false, $callback);
 
-        register_shutdown_function(array($this, 'onShutdown'), $WConnection);
-
-        while (count($channel->callbacks) > 0) {
-            $channel->wait();
+        
+        if($check) {
+            register_shutdown_function(array($this, 'onShutdown'), $WConnection);
+            while (count($channel->callbacks) > 0) {
+                $channel->wait();
+            }
         }
     }
 
