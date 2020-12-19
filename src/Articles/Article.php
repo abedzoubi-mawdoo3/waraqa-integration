653168ebd408432089e1a2c82e296398293196c4
Tareq Istanbouli <tareq.istanbouli@mawdoo3.com>
2020-12-19 21:10:42 +0200

    Save Token on server side ,add title,  merge article ,

--- src/Articles/Article.php
+++ src/Articles/Article.php
@@ -1,5 +1,5 @@
 <?php
-# Test Tagging
+
 namespace Waraqa\Articles;
 
 use GuzzleHttp\Client as GuzzleClient;
@@ -7,44 +7,44 @@ use GuzzleHttp\Client as GuzzleClient;
 class Article
 {
     const GET_ARTICLE_URI = 'client-api/articles/content/';
-
+    
     const PUBLISH_ARTICLE_URI = 'client-api/articles/publish';
-
+    
     /**
      * base_uri
      *
      * @var mixed
      */
     private $base_uri;
-
+    
     /**
      * client_access_id
      *
      * @var mixed
      */
     private $client_access_id;
-
+    
     /**
      * client_password
      *
      * @var mixed
      */
     private $client_password;
-
+    
     /**
      * client
      *
      * @var mixed
      */
     private $client;
-
+    
     /**
      * token
      *
      * @var mixed
      */
     private $token;
-
+    
     /**
      * __construct
      *
@@ -56,12 +56,12 @@ class Article
         $this->base_uri = $base_uri;
         $this->client_access_id = $client_access_id;
         $this->client_password = $client_password;
-
-        // Create a client with a base URI        
+        
+        // Create a client with a base URI
         $this->client = new GuzzleClient(['base_uri' => $this->base_uri]);
         $this->token = $this->getClientToken();
     }
-
+    
     /**
      * Get the article from waraqa API
      *
@@ -74,15 +74,14 @@ class Article
         // Send a request to $request_uri
         $response = $this->client->request('GET', $request_uri, [
             'headers' => [
-                'Authorization' => 'Bearer ' . $this->token,
-                'Accept' => 'application/json'
+                'Authorization' => 'Bearer ' . $this->token
             ]
         ]);
-
+        
         $responseContent = json_decode($response->getBody()->getContents());
         return $responseContent->data;
     }
-
+    
     /**
      * Publish the article to update the status in waraqa API
      *
@@ -92,7 +91,7 @@ class Article
     public function publishArticle(Int $article_id)
     {
         $request_uri = self::PUBLISH_ARTICLE_URI;
-
+        
         // Send a request to $request_uri
         $response = $this->client->request('PUT', $request_uri, [
             'headers' => [
@@ -103,11 +102,11 @@ class Article
                 'articles' => [$article_id]
             ]
         ]);
-
+        
         $responseContent = json_decode($response->getBody()->getContents());
         return $responseContent->data;
     }
-
+    
     /**
      * Authenticate waraqa API using client credentials and get token
      *
@@ -115,18 +114,73 @@ class Article
      */
     public function getClientToken()
     {
-        $response = $this->client->request(
-            'POST',
-            'client-api/login',
-            [
-                'form_params' => [
-                    'access_id' => $this->client_access_id,
-                    'password' => $this->client_password,
+        
+        $tokenFile = "waraqaToken.txt";
+        $fh = fopen($tokenFile, 'a+');
+        $token = fread($fh,5000);
+        if(empty($token))
+        {
+            $response = $this->client->request(
+                'POST',
+                'client-api/login',
+                [
+                    'form_params' => [
+                        'access_id' => $this->client_access_id,
+                        'password' => $this->client_password,
+                    ]
                 ]
+                );
+            $responseToken = json_decode($response->getBody()->getContents());
+            $responseToken = $responseToken->data->token;
+            fwrite($fh,$responseToken);
+            return  $responseToken;
+        } else  {
+            return  $token;
+        }
+        fclose($fh);
+    }
+    /**
+     *
+     * @param string $titles json object
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    
+    public function AddUpdateTitles($titles)
+    {
+        $response = $this->client->request('POST',
+            'client-api/titles',
+            [
+                'headers' => [
+                    'Authorization' => 'Bearer ' . $this->token,
+                    'Accept' => 'application/json'
+                ],
+                'json' => $titles
             ]
-        );
-        $responseToken = json_decode($response->getBody()->getContents());
-
-        return $responseToken->data->token;
+            );
+//         $responseContent = json_decode($response->getBody()->getContents());
+//         print_r($responseContent );
+    }
+    
+    /**
+     *
+     * @param string $titles json objec
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function MergeTitles($titles)
+    {
+        $response = $this->client->request(
+            'PUT',
+            'client-api/titles/merge',
+            [
+                'headers' => [
+                    'Authorization' => 'Bearer ' . $this->token,
+                    'Accept' => 'application/json'
+                ],
+                'json' => $titles
+            ]
+            );
+//         $responseContent = json_decode($response->getBody()->getContents());
+//         print_r($responseContent );
     }
 }
+
